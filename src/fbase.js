f3396ed8e06a2b9d570c3da308bdd04f3c1d7971
YunKukPark <a01043340999@gmail.com>
2021-12-05 20:30:03 +0900

    1.0 React + Firebase Setup

---
+++ src/fbase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase/app';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBzy6jcTXfE0kinL8Cv6NJwLVLtsgaDmFw',
+  authDomain: 'nwitter-74962.firebaseapp.com',
+  projectId: 'nwitter-74962',
+  storageBucket: 'nwitter-74962.appspot.com',
+  messagingSenderId: '621049783060',
+  appId: '1:621049783060:web:262aff9654cf8586679801',
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import fbase from './fbase';
+
+console.log(fbase);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
