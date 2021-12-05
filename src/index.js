cd4f354e788f806fca61c5ad45003ee3c4bd3f8b
YunKukPark <a01043340999@gmail.com>
2021-12-05 20:58:47 +0900

    1.2 Router Setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import fbase from './fbase';
-
-console.log(fbase);
 
 ReactDOM.render(
   <React.StrictMode>
